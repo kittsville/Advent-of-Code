eb1bb02ca9af17812cfe8b3457993938c1edc0a7
Kit <kittsville@googlemail.com>
2018-01-15 16:27:01 +0000

    Add spreadsheet division checksum

--- src/main/scala/adventofcode/Day2.scala
+++ src/main/scala/adventofcode/Day2.scala
@@ -1,10 +1,34 @@
 package adventofcode
 
 object Day2 {
-  def checksum(stringSpreadsheet: String): Int = {
-    val spreadsheet = stringSpreadsheet.split("\n").toList.map(_.split("\\s+").toList.map(_.toInt))
+  def corruptionChecksum(stringSpreadsheet: String): Int = {
+    val spreadsheet = parseSpreadsheet(stringSpreadsheet)
 
    spreadsheet.foldLeft(0)((total, currentRow) => total + rowDifference(currentRow))
+  }
+
+  def divisionChecksum(stringSpreadsheet: String): Int = {
+    val spreadsheet = parseSpreadsheet(stringSpreadsheet)
+
+    spreadsheet.foldLeft(0)((total, currentRow) => total + rowDivision(currentRow))
+  }
+
+  private def parseSpreadsheet(stringSpreadsheet: String): List[List[Int]] = {
+    stringSpreadsheet.split("\n").toList.map(_.split("\\s+").toList.map(_.toInt))
+  }
+
+  private def rowDivision(spreadsheetRow: List[Int]): Int = {
+    val sortedRow = spreadsheetRow.sorted
+    val divisibleNumbers = getDivisibleNumbers(sortedRow.head, sortedRow.tail)
+
+    divisibleNumbers._2 / divisibleNumbers._1
+  }
+
+  private def getDivisibleNumbers(denominator: Int, numerators: List[Int]): (Int, Int) = {
+    numerators.filter(_ % denominator == 0) match {
+      case numerator :: _ => (denominator, numerator)
+      case _ => getDivisibleNumbers(numerators.head, numerators.tail)
+    }
   }
 
   private def rowDifference(spreadsheetRow: List[Int]): Int = {
--- src/test/scala/adventofcode/Day2Spec.scala
+++ src/test/scala/adventofcode/Day2Spec.scala
@@ -1,14 +1,22 @@
 package adventofcode
 
 import org.specs2._
-import adventofcode.Day2.checksum
+import adventofcode.Day2.{corruptionChecksum, divisionChecksum}
 
 class Day2Spec extends mutable.Specification {
   "Corruption Checksum specification where" >> {
     "The spreadsheet must sum to 18" >> {
       val input = "5 1 9 5\n7 5 3\n2 4 6 8"
 
-      checksum(input) must_== 18
+      corruptionChecksum(input) must_== 18
+    }
+  }
+
+  "Division Checksum specification where" >> {
+    "The spreadsheet must sum to 9" >> {
+      val input = "5 9 2 8\n9 4 7 3\n3 8 6 5"
+
+      divisionChecksum(input) must_== 9
     }
   }
 }
