b91c7cd662600a1a563af57f28b8f832ca37d388
Kit <kittsville@googlemail.com>
2018-01-15 11:35:39 +0000

    Add inverse captcha tests

--- src/main/scala/adventofcode/Day1.scala
+++ src/main/scala/adventofcode/Day1.scala
@@ -2,6 +2,17 @@ package adventofcode
 
 object Day1 {
   def inverseCaptcha(captcha: String): Int = {
-    3
+    val digitList = captcha.map(_.asDigit).toList
+
+    val circularDigitList = digitList :+ digitList.head
+
+    circularDigitList.foldLeft((0, -1))(addDigits)._1
+  }
+
+  private def addDigits(left: (Int, Int), right: Int): (Int, Int) = {
+    right match {
+      case left._2 => (left._1 + right, right)
+      case _ => (left._1, right)
+    }
   }
 }
--- src/test/scala/adventofcode/Day1Spec.scala
+++ src/test/scala/adventofcode/Day1Spec.scala
@@ -8,5 +8,17 @@ class Day1Spec extends mutable.Specification {
     "1122 sums to 3" >> {
       inverseCaptcha("1122") must_== 3
     }
+
+    "1111 sums to 4" >> {
+      inverseCaptcha("1111") must_== 4
+    }
+
+    "1234 sums to 0" >> {
+      inverseCaptcha("1234") must_== 0
+    }
+
+    "91212129 sums to 9" >> {
+      inverseCaptcha("91212129") must_== 9
+    }
   }
 }
