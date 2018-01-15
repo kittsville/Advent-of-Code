43f27fe3a87984ed9ea05fd7578d6a4b2d78d5c9
Kit <kittsville@googlemail.com>
2018-01-15 12:18:47 +0000

    Add half captcha

--- src/main/scala/adventofcode/Day1.scala
+++ src/main/scala/adventofcode/Day1.scala
@@ -2,12 +2,36 @@ package adventofcode
 
 object Day1 {
   def inverseCaptcha(captcha: String): Int = {
-    val digitList = captcha.map(_.asDigit).toList
+    val digitList = toDigitList(captcha)
 
     val circularDigitList = digitList :+ digitList.head
 
     circularDigitList.foldLeft((0, -1))(addDigits)._1
   }
+
+  def halfCaptcha(captcha: String): Int = {
+    val digitList = toDigitList(captcha)
+
+    val digitPairs = splitFlipZip(digitList)
+
+    digitPairs.foldLeft(0)(sumMatchingPairs)
+  }
+
+  private def splitFlipZip[A](list: List[A]): List[(A, A)] = {
+    list.splitAt(list.length / 2) match {
+      case (left, right) => list zip right ++ left
+    }
+  }
+
+  private def sumMatchingPairs(total: Int, digitPair: (Int, Int)): Int = {
+    if (digitPair._1 == digitPair._2 ) {
+      total + digitPair._1
+    } else {
+      total
+    }
+  }
+
+  private def toDigitList(rawCaptcha: String): List[Int] = rawCaptcha.map(_.asDigit).toList
 
   private def addDigits(left: (Int, Int), right: Int): (Int, Int) = {
     right match {
--- src/test/scala/adventofcode/Day1Spec.scala
+++ src/test/scala/adventofcode/Day1Spec.scala
@@ -1,6 +1,6 @@
 package adventofcode
 
-import adventofcode.Day1.inverseCaptcha
+import adventofcode.Day1.{inverseCaptcha,halfCaptcha}
 import org.specs2._
 
 class Day1Spec extends mutable.Specification {
@@ -19,6 +19,28 @@ class Day1Spec extends mutable.Specification {
 
     "91212129 sums to 9" >> {
       inverseCaptcha("91212129") must_== 9
+    }
+  }
+
+  "Half Captcha specification where" >> {
+    "1212 sums to 6" >> {
+      halfCaptcha("1212") must_== 6
+    }
+
+    "1221 sums to 0" >> {
+      halfCaptcha("1221") must_== 0
+    }
+
+    "123425 sums to 4" >> {
+      halfCaptcha("123425") must_== 4
+    }
+
+    "123123 sums to 12" >> {
+      halfCaptcha("123123") must_== 12
+    }
+
+    "12131415 sums to 4" >> {
+      halfCaptcha("12131415") must_== 4
     }
   }
 }
