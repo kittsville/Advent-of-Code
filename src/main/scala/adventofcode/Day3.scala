d85f9b68f797606e0c5b8684e2492a050cd9e37a
Kit <kittsville@googlemail.com>
2018-01-17 18:19:21 +0000

    Improve type safety

--- src/main/scala/adventofcode/Day3.scala
+++ src/main/scala/adventofcode/Day3.scala
@@ -14,33 +14,32 @@ object Day3 {
 
   def largerThan(threshold: Int): Int = {
     val pointSurroundings = List((1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1))
-    val matrix = new mutable.HashMap[(Int, Int), Int]
+    val matrix = new Matrix
     var value = 1
     var position = (0, 0)
-    var direction = "right"
+    var direction = Direction("right")
     matrix.put(position, value)
 
     while (value <= threshold) {
       position = direction match {
-        case "up"    => position |+| (0, 1)
-        case "right" => position |+| (1, 0)
-        case "down"  => position |+| (0, -1)
-        case "left"  => position |+| (-1, 0)
+        case Direction("up")    => position |+| (0, 1)
+        case Direction("right") => position |+| (1, 0)
+        case Direction("down")  => position |+| (0, -1)
+        case Direction("left")  => position |+| (-1, 0)
       }
-      println("New", position)
+
       value = pointSurroundings.foldLeft(0)((total,offset) => {
-        println(position |+| offset, matrix.getOrElse(position |+| offset, 0))
         total + matrix.getOrElse(position |+| offset, 0)
       })
-      println("Set", position, value)
+
       matrix.put(position, value)
 
       direction = direction match {
-        case "up"    if !matrix.contains(position |+| (-1, 0)) => "left"
-        case "right" if !matrix.contains(position |+| (0, 1))  => "up"
-        case "down"  if !matrix.contains(position |+| (1, 0))  => "right"
-        case "left"  if !matrix.contains(position |+| (0, -1)) => "down"
-        case any                                               => any
+        case Direction("up")    if !matrix.contains(position |+| (-1, 0)) => Direction("left")
+        case Direction("right") if !matrix.contains(position |+| (0, 1))  => Direction("up")
+        case Direction("down")  if !matrix.contains(position |+| (1, 0))  => Direction("right")
+        case Direction("left")  if !matrix.contains(position |+| (0, -1)) => Direction("down")
+        case any                                                          => any
       }
     }
 
@@ -64,4 +63,8 @@ object Day3 {
 
     edgeSquare % (previousDepth * 2)
   }
+
+  private case class Direction(direction: String)
+
+  private class Matrix extends mutable.HashMap[(Int, Int), Int]
 }
