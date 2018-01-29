7fa58729b26d8741eb1433fce5b1a42788de6f80
Kit <kittsville@googlemail.com>
2018-01-29 12:20:44 +0000

    Correct imbalanced robot towers

--- src/main/scala/adventofcode/Day7.scala
+++ src/main/scala/adventofcode/Day7.scala
@@ -1,8 +1,13 @@
 package adventofcode
 
+import scala.collection.immutable.HashMap
+import scala.util.control.NoStackTrace
+
 object Day7 {
   private val parseSupportRobot = "^([a-z]+).*".r
+  private val parseRobotWeight = """^([a-z]+) [(](\d+)[)].*""".r
   private val parseSupportedRobots = ".*-> ([a-z ,]+)$".r
+  private val parseMapping = """^([a-z]+) [(]\d+[)] -> ([a-z ,]+)$""".r
 
   def getBottom(tower: List[String]): String = {
     val supports = tower.filterNot(_.endsWith(")"))
@@ -10,6 +15,57 @@ object Day7 {
     val supportedRobots  = supports.foldLeft(Set[String]())(getSupportedRobots)
 
     (supportingRobots -- supportedRobots).lastOption.get
+  }
+
+  def getCorrectWeight(tower: List[String]): Int = {
+    val supports = tower.filterNot(_.endsWith(")"))
+    val supportingRobots = supports.foldLeft(Set[String]())(getSupportingRobots)
+    val supportedRobots  = supports.foldLeft(Set[String]())(getSupportedRobots)
+    val topRobot = (supportingRobots -- supportedRobots).lastOption.get
+
+    implicit val weights: Map[String, Int] = tower.map(getRobotWeights).toMap
+    implicit val mapping: Map[String, List[String]] = tower.map(getRobotChildren).reduce(_ ++ _)
+
+    try {
+      getWeightFromTower(topRobot)._2
+    } catch {
+      case e: ImbalanceFound => e.correctWeight
+    }
+  }
+
+  private def getWeightFromTower(robot: String)(implicit weights: Map[String, Int], mapping: Map[String, List[String]]): (Int, Int) = {
+    val childWeights = mapping(robot).map(getWeightFromTower)
+
+    if (childWeights.nonEmpty) {
+      val weightFrequency = childWeights.groupBy(_._2)
+      val mostCommonWeight = weightFrequency.maxBy(_._2.size)._1
+      val leastCommonWeight = weightFrequency.minBy(_._2.size)._2.head
+      val weightDifference = mostCommonWeight - leastCommonWeight._2
+
+      if (weightDifference != 0) {
+        throw ImbalanceFound(leastCommonWeight._1 + weightDifference)
+      }
+    }
+
+    val robotWeight = weights(robot)
+    (robotWeight, robotWeight + childWeights.foldLeft(0)((total, child) => total + child._2))
+  }
+
+  case class ImbalanceFound(correctWeight:Int) extends Exception with NoStackTrace
+
+  private def getRobotWeights(rawRobot: String): (String, Int) = {
+    rawRobot match {
+      case parseRobotWeight(name, rawWeight) => (name, rawWeight.toInt)
+      case _ => throw new Exception("Invalid robot weight format")
+    }
+  }
+
+  private def getRobotChildren(rawMapping: String): Map[String, List[String]] = {
+    rawMapping match {
+      case parseMapping(robot, rawChildren) => HashMap((robot, rawChildren.split(", ").toList))
+      case parseSupportRobot(robot) => HashMap((robot, List()))
+      case _ => throw new Exception("Invalid robot format")
+    }
   }
 
   private def getSupportingRobots(supportingRobots: Set[String], rawSupport: String): Set[String] = {
--- src/test/scala/adventofcode/Day7Spec.scala
+++ src/test/scala/adventofcode/Day7Spec.scala
@@ -1,6 +1,6 @@
 package adventofcode
 
-import adventofcode.Day7.getBottom
+import adventofcode.Day7.{getBottom, getCorrectWeight}
 import org.specs2._
 
 class Day7Spec extends mutable.Specification {
@@ -42,6 +42,39 @@ class Day7Spec extends mutable.Specification {
       )
 
       getBottom(input) must_== "tknk"
+    }
+  }
+
+  "Tower balancer specification where" >> {
+    "Only one disk is unbalanced" >> {
+      val input = List(
+        "ktlj (5)",
+        "polc (5)",
+        "fwft (6)",
+        "ajbf (5) -> ktlj, polc, fwft"
+      )
+
+      getCorrectWeight(input) must_== 5
+    }
+
+    "One of several disks is unbalanced" >> {
+      val input = List(
+        "pbga (66)",
+        "xhth (57)",
+        "ebii (61)",
+        "havc (66)",
+        "ktlj (57)",
+        "fwft (72) -> ktlj, cntj, xhth",
+        "qoyq (66)",
+        "padx (45) -> pbga, havc, qoyq",
+        "tknk (41) -> ugml, padx, fwft",
+        "jptl (61)",
+        "ugml (68) -> gyxo, ebii, jptl",
+        "gyxo (61)",
+        "cntj (57)"
+      )
+
+      getCorrectWeight(input) must_== 60
     }
   }
 }
