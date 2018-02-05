396738fc7b7504e5ce894ceb3a93ebb36213b4ce
Kit <kittsville@googlemail.com>
2018-02-05 18:04:59 +0000

    Add incrementing conditional on registry comparison

--- src/main/scala/adventofcode/Day8.scala
+++ src/main/scala/adventofcode/Day8.scala
@@ -3,23 +3,47 @@ package adventofcode
 import scala.collection.mutable
 
 object Day8 {
-  private val parseInstruction = """^([a-z]+) (inc|dec) (-?\d+).*""".r
+  private val parseInstruction = """^([a-z]+) (inc|dec) (-?\d+) if ([a-z]+) (==|!=|>=|<=|>|<) (-?\d+)$""".r
 
   def largestRegister(instructions: List[String]): Int = {
     if (instructions.isEmpty) {
       return 0
     }
 
-    var registry = mutable.HashMap[String, Int]().withDefaultValue(0)
+    implicit val registry: mutable.Map[String, Int] = mutable.HashMap[String, Int]().withDefaultValue(0)
+
 
     instructions.foreach {
       {
-        case parseInstruction(register, "inc", offset) => registry(register) = registry(register) + offset.toInt
-        case parseInstruction(register, "dec", offset) => registry(register) = registry(register) - offset.toInt
+        case parseInstruction(register, "inc", offset, cmpRegister, comparison, compareTo) if conditionHolds(cmpRegister, comparison, compareTo.asInt) => registry.put(register, registry(register) + offset.asInt)
+        case parseInstruction(register, "dec", offset) => registry.put(register, registry(register) - offset.asInt)
+        case parseInstruction(_, "inc", _, _, _, _) =>
+        case parseInstruction(_, "dec", _, _, _, _) =>
         case invalid => throw new MatchError(s"Invalid instruction: '$invalid'")
       }
     }
 
-    registry.values.max
+    registry.size match {
+      case 0 => 0
+      case _ => registry.values.max
+    }
+  }
+
+  private def conditionHolds(register: String, comparison: String, compareTo: Int)(implicit registry: mutable.Map[String, Int]): Boolean = {
+    comparison match {
+      case "=="  => registry(register) == compareTo
+      case ">"   => registry(register) > compareTo
+      case "<"   => registry(register) < compareTo
+      case ">="  => registry(register) >= compareTo
+      case "<="  => registry(register) <= compareTo
+      case "!="  => registry(register) != compareTo
+      case invalid => throw new MatchError(s"Invalid comparator $invalid")
+    }
+  }
+
+  private implicit class StringToInt(x: String) {
+    def asInt: Int = {
+      Integer.parseInt(x, 10)
+    }
   }
 }
--- src/test/scala/adventofcode/Day8Spec.scala
+++ src/test/scala/adventofcode/Day8Spec.scala
@@ -5,7 +5,7 @@ import org.specs2._
 
 class Day8Spec extends mutable.Specification {
   "Register specification where" >> {
-    "No instructions are executed" >> {
+    "There are no instructions" >> {
       largestRegister(List()) must_== 0
     }
 
@@ -20,6 +20,60 @@ class Day8Spec extends mutable.Specification {
       )
 
       largestRegister(instructions) must_== 4
+    }
+
+    "An instruction is dependant on a condition" >> {
+      largestRegister(List("a inc 5 if a == 5")) must_== 0
+    }
+
+    "Multiples instructions are dependant on conditions" >> {
+      "Greater than" >> {
+        val instructions = List(
+          "a inc 2 if a > 0",
+          "a inc 3 if a == 0",
+          "a inc 2 if a > 2"
+        )
+
+        largestRegister(instructions) must_== 5
+      }
+
+      "Greater than or equals" >> {
+        val instructions = List(
+          "a inc 2 if a >= 0",
+          "a inc 2 if a >= 1",
+          "a inc 3 if a >= 5"
+        )
+
+        largestRegister(instructions) must_== 4
+      }
+
+      "Less than" >> {
+        val instructions = List(
+          "a inc 2 if a < 0",
+          "a inc 3 if a < 1"
+        )
+
+        largestRegister(instructions) must_== 3
+      }
+
+      "Less than or equals" >> {
+        val instructions = List(
+          "a inc 2 if a <= 0",
+          "a inc 2 if a <= 3",
+          "a inc 3 if a <= 3"
+        )
+
+        largestRegister(instructions) must_== 4
+      }
+
+      "Not equals" >> {
+        val instructions = List(
+          "a inc 2 if a != 2",
+          "a inc 2 if a != 2"
+        )
+
+        largestRegister(instructions) must_== 2
+      }
     }
   }
 }
